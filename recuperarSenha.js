69c67816bf5942cf5b38a4d8bba7beda814ecaa9
LoboMurilo <112902226+LoboMurilo@users.noreply.github.com>
2022-11-16 15:05:15 -0300

    recuperar senha JS
    Funcionalidades prontas de validação da recuperação da senha

---
+++ recuperarSenha.js
@@ -0,0 +1,51 @@
+//  var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+var inUsuario = document.getElementById('inUsuario');
+var inSenha = document.getElementById('inSenha');
+var inSenha2 = document.getElementById('inSenha2');
+var outResposta = document.getElementById('outResposta');
+var resposta = document.createElement('h3');
+
+
+function senhaNova(e) {
+    var senha1 = inSenha.value;
+    var senha2 = inSenha2.value;
+
+    e.preventDefault();
+    resposta.textContent = "";
+
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+    if (!reg.test(inUsuario.value)) {
+        outResposta.append(resposta);
+        resposta.textContent += "Usuário Incorreto. " + "\n";
+        resposta.style.color = "red";
+    }
+    if (senha1.length < 6 || senha2.length < 6) {
+        outResposta.append(resposta);
+        resposta.textContent += "Nova senha deve conter no mínimo 6 caracteres e campo não pode estar em branco" + "\n";
+        resposta.style.color = "red";
+    }if(inSenha.value != inSenha2.value && inSenha2.value != inSenha.value){
+        outResposta.append(resposta);
+        resposta.textContent += "Confirme as senhas digitadas" + "\n";
+        resposta.style.color = "red";
+    }
+    
+    else{
+        if(reg.test(inUsuario.value) && (inSenha.value == inSenha2.value)){
+
+            outResposta.append(resposta);
+            resposta.textContent = "Nova Senha Cadastrada!";
+            resposta.style.color = "green";
+        }
+        
+    }
+
+
+
+    // inUsuario.value = "";
+
+}
+
+var btRecuperar = document.getElementById('btRecuperar');
+btRecuperar.addEventListener("click", senhaNova);
